652dbf406e33b8a1e1b0dde5e04e94dcd4e1263d
Park KyungHwa <kyunghwa.park123@gmail.com>
2018-12-04 15:50:57 +0900

    Digital

---
+++ test_digital/test_digital.ino
@@ -0,0 +1,17 @@
+int a;
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  a=digitalRead(13);
+  if(a==HIGH) {
+    Serial.println("a is HIGH");
+  } else {
+    Serial.println("a is LOW");
+  }
+  delay(500);
+}
