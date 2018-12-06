78680f99356acb3576272a6066d83a278c6107ef
KyungHwa Park <kyunghwa.park123@gmail.com>
2018-12-06 17:23:57 +0900

    wink animation
    LCD창을 활용한 아주 간단한 윙크 애니메이션

---
+++ wink_animation/wink_animation.ino
@@ -0,0 +1,24 @@
+// LCD로 wink 애니메이션 표현하기
+
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
+
+LiquidCrystal_I2C lcd(0x27, 16, 2);
+
+void setup() {
+  lcd.begin();
+  lcd.backlight();
+}
+
+void loop() {
+  lcd.print("(^o^) @ (^_-)");
+  delay(1000);
+  lcd.home();
+  lcd.clear();
+  
+  lcd.print("(-_^) @ (^o^)");
+  delay(1000);
+  lcd.home();
+  lcd.clear();
+
+}
